db7f640067700d26aa6a11b0d17475a12701744a
TX <3418013863@qq.com>
2025-11-14 14:25:52 +0800

    refactor UserController to streamline imports and enhance readability

--- src/main/java/com/asus/tikra/controller/UserController.java
+++ src/main/java/com/asus/tikra/controller/UserController.java
@@ -9,23 +9,15 @@ import com.asus.tikra.exception.BusinessException;
 import com.asus.tikra.exception.ErrorCode;
 import com.asus.tikra.exception.ThrowUtils;
 import com.asus.tikra.model.dto.*;
+import com.asus.tikra.model.entity.User;
 import com.asus.tikra.model.vo.LoginUserVO;
 import com.asus.tikra.model.vo.UserVO;
+import com.asus.tikra.service.UserService;
 import com.mybatisflex.core.paginate.Page;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.BeanUtils;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.asus.tikra.model.entity.User;
-import com.asus.tikra.service.UserService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
